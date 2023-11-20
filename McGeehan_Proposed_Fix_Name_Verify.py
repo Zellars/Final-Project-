8a1ef1f7fd2aeb21ac38d4299f0675d93fc5f938
MkGee <matthewrmcgeehan2@gmail.com>
2023-11-20 14:29:37 -0500

    Create McGeehan_Proposed_Fix_Name_Verify.py

---
+++ McGeehan_Proposed_Fix_Name_Verify.py
@@ -0,0 +1,22 @@
+print("Hello Player 1! What is your name?")
+while True:
+    user_name = input()
+    confirm = input(f"You entered {user_name}. Is this correct? (yes/no) ")
+    if confirm.lower().startswith('y'):
+        break
+    else:
+        print("Please re-enter your name.")
+
+print("Hello, {}!".format(user_name))
+
+print("Are you a boy or girl?")
+while True:
+    user_gender = input()
+    confirm = input(f"You entered {user_gender}. Is this correct? (yes/no) ")
+    if confirm.lower().startswith('y'):
+        break
+    else:
+        print("Please re-enter your gender.")
+
+print("Is this correct?")
+print(f"You are {user_name} and you are a {user_gender}!")
