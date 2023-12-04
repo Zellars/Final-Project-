a0e5cff1bb9a712cab99070a5134c3746b9b3cea
Antoniolee44 <143443045+Antoniolee44@users.noreply.github.com>
2023-12-04 13:46:52 -0500

    Add files via upload

---
+++ Orgeon trail project MODEL 3.py
@@ -0,0 +1,43 @@
+print("Hello Player 1! What is your name?")
+def information(): # function callback
+    return biography()
+
+def biography():
+while True:
+    user_name = input()
+    confirm = input(f"You entered {user_name}. Is this correct? (yes/no) ")
+    if confirm.lower() != 'yes':
+        continue
+    break
+# allows for user to change answer of name if mistakes are made or if they simply want to change it confirm.lower()
+# makes it so that if the user types "Yes" the code will still be recognized even if the options are given in lowercase
+
+
+print("Hello, {}!".format(user_name))
+# gets username and stores it under user_name
+
+print("Are you a boy or girl?")
+while True:
+    user_gender = input()
+    confirm = input(f"You entered {user_gender}. Is this correct? (yes/no) ")
+    if confirm.lower().startswith('y'):  # did the player say yes?
+        break
+    else:
+        print("Please re-enter your gender.")
+
+# final preparations before adventure
+print("Is this correct?")
+print("You are " + user_name + " and you are a " + user_gender + "?")
+print("(yes/no)")
+# checks if player says yes or no
+while True:
+    correct = input()
+    if correct.lower().startswith('y'):
+        break
+    else:
+        print('please re-enter your information')
+        
+# returns player for redoing of information
+# final message
+print(f'Welcome to your new adventure, {user_name}!')
+print('Let\'s get started.')
